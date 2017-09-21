cf043dc1c6153f17e92152844aa5ddad58613e48
Archie Yang <archie.yang@outlook.com>
2017-09-21 23:31:13 +0800

    Add CharacterEncoding

--- src/PcscDotNet/IPcscProvider.cs
+++ src/PcscDotNet/IPcscProvider.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Text;
+
 namespace PcscDotNet
 {
     public interface IPcscProvider
     {
+        Encoding CharacterEncoding { get; }
+
         unsafe SCardError SCardEstablishContext(SCardScope dwScope, void* pvReserved1, void* pvReserved2, SCardContext* phContext);
 
         unsafe SCardError SCardFreeMemory(SCardContext hContext, void* pvMem);
--- src/PcscDotNet/WinSCard.cs
+++ src/PcscDotNet/WinSCard.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace PcscDotNet
 {
     public class WinSCard : IPcscProvider
     {
         public const string DllName = "WinSCard.dll";
+
+        public static Encoding CharacterEncoding => Encoding.Unicode;
+
+        Encoding IPcscProvider.CharacterEncoding => CharacterEncoding;
 
         [DllImport(DllName)]
         public unsafe static extern SCardError SCardEstablishContext(SCardScope dwScope, void* pvReserved1, void* pvReserved2, SCardContext* phContext);
