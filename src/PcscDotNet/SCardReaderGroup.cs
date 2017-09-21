e4ec5183caf372819bf4705edb3d8289524c16cb
Archie Yang <archie.yang@outlook.com>
2017-09-21 23:22:44 +0800

    Specify values

--- src/PcscDotNet/SCardReaderGroup.cs
+++ src/PcscDotNet/SCardReaderGroup.cs
@@ -9,17 +9,17 @@ namespace PcscDotNet
         /// Group used when no group name is provided when listing readers.
         /// Returns a list of all readers, regardless of what group or groups the readers are in.
         /// </summary>
-        All,
+        All = 0,
         /// <summary>
         /// Default group to which all readers are added when introduced into the system.
         /// </summary>
-        Defualt,
+        Defualt = 1,
         /// <summary>
         /// Unused legacy value.
         /// This is an internally managed group that cannot be modified by using any reader group APIs.
         /// It is intended to be used for enumeration only.
         /// </summary>
-        Local,
+        Local = 2,
         /// <summary>
         /// List all readers in the system.
         /// (Same as All.)
@@ -30,6 +30,6 @@ namespace PcscDotNet
         /// This is an internally managed group that cannot be modified by using any reader group APIs.
         /// It is intended to be used for enumeration only.
         /// </summary>
-        System
+        System = 3
     }
 }
