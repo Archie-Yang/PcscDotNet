c1d6e9f4d4213d379d3233122ad2c524e767bcbd
Archie Yang <archie.yang@outlook.com>
2017-09-22 00:03:53 +0800

    Update comment

--- src/PcscDotNet/PcscProvider.cs
+++ src/PcscDotNet/PcscProvider.cs
@@ -1,7 +1,7 @@
 namespace PcscDotNet
 {
     /// <summary>
-    /// Usage shared for IPcscProvider
+    /// Usage shared for IPcscProvider.
     /// </summary>
     public static class PcscProvider
     {
