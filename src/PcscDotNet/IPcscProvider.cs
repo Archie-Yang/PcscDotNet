169c051f640c89eaadb5c07bcd7ba070dc00c6a3
Archie Yang <archie.yang@outlook.com>
2017-09-21 22:36:55 +0800

    Add SCardFreeMemory method

--- src/PcscDotNet/IPcscProvider.cs
+++ src/PcscDotNet/IPcscProvider.cs
@@ -4,6 +4,8 @@ namespace PcscDotNet
     {
         unsafe SCardError SCardEstablishContext(SCardScope dwScope, void* pvReserved1, void* pvReserved2, SCardContext* phContext);
 
+        unsafe SCardError SCardFreeMemory(SCardContext hContext, void* pvMem);
+
         SCardError SCardIsValidContext(SCardContext hContext);
 
         SCardError SCardReleaseContext(SCardContext hContext);
--- src/PcscDotNet/WinSCard.cs
+++ src/PcscDotNet/WinSCard.cs
@@ -11,6 +11,9 @@ namespace PcscDotNet
         public unsafe static extern SCardError SCardEstablishContext(SCardScope dwScope, void* pvReserved1, void* pvReserved2, SCardContext* phContext);
 
         [DllImport(DllName)]
+        public unsafe static extern SCardError SCardFreeMemory(SCardContext hContext, void* pvMem);
+
+        [DllImport(DllName)]
         public static extern SCardError SCardIsValidContext(SCardContext hContext);
 
         [DllImport(DllName)]
@@ -19,6 +22,11 @@ namespace PcscDotNet
         unsafe SCardError IPcscProvider.SCardEstablishContext(SCardScope dwScope, void* pvReserved1, void* pvReserved2, SCardContext* phContext)
         {
             return SCardEstablishContext(dwScope, pvReserved1, pvReserved2, phContext);
+        }
+
+        unsafe SCardError IPcscProvider.SCardFreeMemory(SCardContext hContext, void* pvMem)
+        {
+            return SCardFreeMemory(hContext, pvMem);
         }
 
         SCardError IPcscProvider.SCardIsValidContext(SCardContext hContext)
