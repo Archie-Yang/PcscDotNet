c17c806fb0ee84a9f3c5f8b65e0670ca80948296
Archie Yang <archie.yang@outlook.com>
2017-09-30 11:36:02 +0800

    Add `PcscReaderStatus` class

---
+++ src/PcscDotNet/PcscReaderStatus.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace PcscDotNet
+{
+    public delegate void PcscReaderStatusAction(PcscReaderStatus readerStatus);
+
+    public abstract class PcscReaderStatus : ReadOnlyCollection<PcscReaderState>
+    {
+        /// <summary>
+        /// This special reader name is used to be notified when a reader is added to or removed from the system through `SCardGetStatusChange`.
+        /// </summary>
+        public const string PnPReaderName = "\\\\?PnP?\\Notification";
+
+        protected readonly PcscContext Context;
+
+        public PcscReaderStatus(PcscContext context, IList<string> readerNames) : base(new List<PcscReaderState>(readerNames.Count))
+        {
+            Context = context;
+            foreach (var readerName in readerNames)
+            {
+                Items.Add(new PcscReaderState() { ReaderName = readerName });
+            }
+        }
+
+        public PcscReaderStatus Do(PcscReaderStatusAction action)
+        {
+            action(this);
+            return this;
+        }
+
+        public abstract PcscReaderStatus WaitForChanged(int timeout = Timeout.Infinite);
+    }
+}
