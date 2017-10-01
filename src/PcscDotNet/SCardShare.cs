a86ab3c4f56f2a902f1e5f85234067d4e4ace08b
Archie Yang <archie.yang@outlook.com>
2017-10-01 22:43:59 +0800

    Add `SCardShare` enumeration

---
+++ src/PcscDotNet/SCardShare.cs
@@ -0,0 +1,21 @@
+namespace PcscDotNet
+{
+    /// <summary>
+    /// Indicates whether other applications may form connections to the card.
+    /// </summary>
+    public enum SCardShare
+    {
+        /// <summary>
+        /// This application demands direct control of the reader, so it is not available to other applications.
+        /// </summary>
+        Direct = 3,
+        /// <summary>
+        /// This application is not willing to share this card with other applications.
+        /// </summary>
+        Exclusive = 1,
+        /// <summary>
+        /// This application is willing to share this card with other applications.
+        /// </summary>
+        Shared = 2
+    }
+}
