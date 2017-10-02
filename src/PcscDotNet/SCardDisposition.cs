26ebb7b659a6fb5214d77d9b0f8e191fd40808ca
Archie Yang <archie.yang@outlook.com>
2017-10-02 22:32:17 +0800

    Add `SCardDisposition` enumeration

---
+++ src/PcscDotNet/SCardDisposition.cs
@@ -0,0 +1,25 @@
+namespace PcscDotNet
+{
+    /// <summary>
+    /// Action to take on the card in the connected reader on close.
+    /// </summary>
+    public enum SCardDisposition
+    {
+        /// <summary>
+        /// Eject the card on close.
+        /// </summary>
+        Eject = 3,
+        /// <summary>
+        /// Don't do anything special on close.
+        /// </summary>
+        Leave = 0,
+        /// <summary>
+        /// Reset the card on close.
+        /// </summary>
+        Reset = 1,
+        /// <summary>
+        /// Power down the card on close.
+        /// </summary>
+        Unpower = 2
+    }
+}
