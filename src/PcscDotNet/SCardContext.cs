f8e2bb07116d38a50035e07e0880ce443dfe860e
Archie Yang <archie.yang@outlook.com>
2017-10-02 22:22:10 +0800

    Update comment

--- src/PcscDotNet/SCardContext.cs
+++ src/PcscDotNet/SCardContext.cs
@@ -1,11 +1,23 @@
 namespace PcscDotNet
 {
+    /// <summary>
+    /// A handle that identifies the resource manager context.
+    /// </summary>
     public struct SCardContext
     {
+        /// <summary>
+        /// Default value.
+        /// </summary>
         public static readonly SCardContext Default = default(SCardContext);
 
+        /// <summary>
+        /// Returns true if `Value` is not null; otherwise, false.
+        /// </summary>
         public unsafe bool HasValue => Value != null;
 
+        /// <summary>
+        /// Handle value.
+        /// </summary>
         public unsafe void* Value;
     }
 }
