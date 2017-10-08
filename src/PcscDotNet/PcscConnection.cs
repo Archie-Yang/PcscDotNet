7f1e9c22894de895e5b12e1e1647d0053edb11d1
Archie Yang <archie.yang@outlook.com>
2017-10-08 13:09:34 +0800

    Add properties and methods about connection. Implement `IDisposable` interface.

--- src/PcscDotNet/PcscConnection.cs
+++ src/PcscDotNet/PcscConnection.cs
@@ -1,8 +1,18 @@
+using System;
+
 namespace PcscDotNet
 {
-    public class PcscConnection
+    public class PcscConnection : IDisposable
     {
         public PcscContext Context { get; private set; }
+
+        public SCardHandle Handle { get; private set; }
+
+        public bool IsConnect => Handle.HasValue;
+
+        public bool IsDisposed { get; private set; } = false;
+
+        public SCardProtocols Protocols { get; private set; } = SCardProtocols.Undefined;
 
         public IPcscProvider Provider { get; private set; }
 
@@ -12,6 +22,43 @@ namespace PcscDotNet
         {
             Provider = (Context = context).Provider;
             ReaderName = readerName;
+        }
+
+        ~PcscConnection()
+        {
+            Dispose();
+        }
+
+        public unsafe PcscConnection Connect(SCardShare shareMode, SCardProtocols protocols, PcscExceptionHandler onException = null)
+        {
+            SCardHandle handle;
+            Provider.SCardConnect(Context.Handle, ReaderName, shareMode, protocols, &handle, &protocols).ThrowIfNotSuccess(onException);
+            Handle = handle;
+            Protocols = protocols;
+            return this;
+        }
+
+        public PcscConnection Disconnect(SCardDisposition disposition = SCardDisposition.Leave, PcscExceptionHandler onException = null)
+        {
+            if (IsDisposed) throw new ObjectDisposedException(nameof(PcscConnection), nameof(Disconnect));
+            DisconnectInternal(disposition, onException);
+            return this;
+        }
+
+        public void Dispose()
+        {
+            if (IsDisposed) return;
+            DisconnectInternal();
+            IsDisposed = true;
+            GC.SuppressFinalize(this);
+        }
+
+        private void DisconnectInternal(SCardDisposition disposition = SCardDisposition.Leave, PcscExceptionHandler onException = null)
+        {
+            if (!IsConnect) return;
+            Provider.SCardDisconnect(Handle, disposition).ThrowIfNotSuccess(onException);
+            Handle = SCardHandle.Default;
+            Protocols = SCardProtocols.Undefined;
         }
     }
 }
