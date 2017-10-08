caff4c9b17a242e642df4b9beee887eb69c5c422
Archie Yang <archie.yang@outlook.com>
2017-10-08 14:09:53 +0800

    Add `SCardShare.Undefined`

--- src/PcscDotNet/SCardShare.cs
+++ src/PcscDotNet/SCardShare.cs
@@ -16,6 +16,10 @@ namespace PcscDotNet
         /// <summary>
         /// This application is willing to share this card with other applications.
         /// </summary>
-        Shared = 2
+        Shared = 2,
+        /// <summary>
+        /// Share mode is undefined, can not be used to connect to card/reader.
+        /// </summary>
+        Undefined = 0
     }
 }
